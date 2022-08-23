41eca46c8f2481b7b69e20a7722793708915f51a
KozmikNano <77017394+KozmikNano@users.noreply.github.com>
2022-08-22 22:46:07 -0400

    Update sw.js

--- static/sw.js
+++ static/sw.js
@@ -1,10 +1,10 @@
 importScripts('https://arc.io/arc-sw-core.js')
-//importScripts('./uv/uv.sw.js');
+importScripts('./uv/uv.sw.js');
 
-//7const sw = new UVServiceWorker();
+const sw = new UVServiceWorker();
 
-//self.addEventListener('fetch', event =>
-    //event.respondWith(
-    //    sw.fetch(event)
-  //  )
-//);
+self.addEventListener('fetch', event =>
+    event.respondWith(
+        sw.fetch(event)
+    )
+);
